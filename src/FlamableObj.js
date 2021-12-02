1ef660341ff6005fe7036b1b0133b7f8c9b5da75
Peter Smid <petosmid@gmail.com>
2021-12-02 13:45:25 +0100

    refractor

--- src/FlamableObj.js
+++ src/FlamableObj.js
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
 import { colord } from "colord";
 import FireEmitter from "./FireEmitter";
 import TextLabel from "./TextLabel";
+import SmokeEmitter from "./SmokeEmitter";
 const ENV_TEMPERATURE = 20; // degrees celsius
 
 export default class FlammableObj extends PIXI.Container {
@@ -11,40 +12,43 @@ export default class FlammableObj extends PIXI.Container {
     this.x = position.x;
     this.y = position.y;
     this.sizeModifiier = Math.random() * 20 - 10;
-    
+
     this.sprite = new PIXI.Sprite(PIXI.Texture.from(texture));
     this.sprite.anchor.set(0.5);
     this.sprite.angle = Math.random() * 360;
     this.sprite.width = (64 + this.sizeModifiier) * scale;
     this.sprite.height = (64 + this.sizeModifiier) * scale;
-    
+
     this.interactive = true;
     this.debug = true;
     // fire properties
     this._temperature = ENV_TEMPERATURE; // start temperature
+    this.tempRisePerTickLimit = 10;
+    this.plannedTempRise = [];
     this.decay = 0.5; // degrees celsius per update
-    this.burnSpeed = 1 * scale;
-    this.combustionStartTreshold = 300; // degrees celsius
+    this.burnSpeed = 0.5 * scale;
+    this.combustionStartTreshold = 300 + (Math.random() * 100); // degrees celsius
     this.maxCombustionTemperature = 600; // degress celsius
-    this._fuelTotal = 200 * scale; // amount of total fuel, no unit
-    this._fuelCurrent = 200 * scale; // amount of fuel remaining
+    this._fuelTotal = 200 * scale + (Math.random() * 100); // amount of total fuel, no unit
+    this._fuelCurrent = this._fuelTotal; // amount of fuel remaining
     this._fireStrength = 0;
     this.burntRatio = 0;
     this.on("pointerdown", this.setOnFire);
     this.neighbors = [];
     this.fireEmitter = new FireEmitter();
+    this.smokeEmitter = new SmokeEmitter();
 
     this.textLabel = new TextLabel("");
-    this.textLabel.anchor = 0.5
+    this.textLabel.anchor = 0.5;
 
     this.init();
     //this.radiance = 0;   // 0 - 100
     //this.filter = new PIXI.Filter(ColorOverlayFilter);
   }
-  
+
   init = () => {
     this.fireEmitter.updateOwnerPos(this.x, this.y);
-
+    this.smokeEmitter.updateOwnerPos(this.x, this.y);
     this.addChild(this.sprite);
     this.addChild(this.textLabel);
   };
@@ -65,15 +69,18 @@ export default class FlammableObj extends PIXI.Container {
   };
 
   update = () => {
-    if (this.debug) {
-      this.textLabel.text = Math.round(this.temperature);
-    } else {
-      this.textLabel.text = "";
-    }
+    this.dumpPlannedTempRise();
+    this.updateDebugText();
 
+    if (this.burntRatio === 1) {
+      this.updateSmokeStrength();
+    }
+    
     const shouldBurn = this.shouldActivelyBurn();
     if (shouldBurn) {
+      this.smokeEmitter.emit = true;
       this.fireEmitter.emit = true;
+      this.updateFireProps();
       this.heatUp();
       this.consumeFuel();
       this.calculateBurntRatio();
@@ -83,6 +90,35 @@ export default class FlammableObj extends PIXI.Container {
       this.fireEmitter.emit = false;
       this.decayTemperature();
     }
+  };
+
+  updateSmokeStrength = () => {
+    const chance = (Math.round((this.temperature / this.maxCombustionTemperature) * 100) / 100) / 8
+    this.smokeEmitter.spawnChance = (chance > 0.02) ? chance : 0
+  }
+
+  updateDebugText = () => {
+    if (this.debug) {
+      this.textLabel.text = Math.round(this.temperature);
+    } else {
+      this.textLabel.text = "";
+    }
+  }
+  
+  updateFireProps = () => {
+    const spawnChance = this.fireStrength + 0.1
+    this.fireEmitter.spawnChance = spawnChance;
+  }
+
+  dumpPlannedTempRise = () => {
+    const total = this.plannedTempRise.reduce((total, current) => total + current, 0)
+
+    if (total > this.tempRisePerTickLimit) {
+      this.temperature += this.tempRisePerTickLimit;
+    } else {
+      this.temperature += total;
+    }
+    this.plannedTempRise = [];
   };
 
   set fuel(amount) {
@@ -131,13 +167,15 @@ export default class FlammableObj extends PIXI.Container {
   };
 
   setOnFire = () => {
-    this.temperature = 300;
+    this.temperature = 450;
   };
 
   raiseTemperature = (degrees) => {
-    if (!this.fuel) return;
+    //if (!this.fuel) return;
     if (this.temperature <= this.maxCombustionTemperature) {
-      this.temperature = this.temperature + degrees;
+      // will only receive half the heat from next tree per tick
+      const reducedTemp = degrees / (this.plannedTempRise.length + 1)
+      this.plannedTempRise.push(reducedTemp);
     }
   };
 
