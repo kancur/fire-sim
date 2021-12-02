80d224d364f3bc96f34d6f115f7a0c01e1afe585
Peter Smid <petosmid@gmail.com>
2021-12-02 13:45:35 +0100

    initial work

--- src/index.js
+++ src/index.js
@@ -1,59 +1,69 @@
-import * as PIXI from "pixi.js";
-import * as particles from "@pixi/particle-emitter";
-import FlammableObj from "./FlamableObj";
-import FlammableArea from "./FlammableArea";
-import gameLoop from "./gameLoop";
-import Fire from "./assets/fire.png";
-import FireEmitter from "./FireEmitter";
-import { particleContainer } from "./particleContainer";
-import ForestGenerator from "./ForestGenerator";
-import "./style.css";
-import SettingsHandlers from "./SettingsHandlers";
+import * as PIXI from 'pixi.js';
+import FlammableArea from './FlammableArea';
+import gameLoop from './gameLoop';
+import { particleContainer } from './particleContainer';
+import ForestGenerator from './ForestGenerator';
+import './style.css';
+import RangeInput from './inputs/RangeInput';
+import CheckboxInput from './inputs/CheckBox';
+import ButtonInput from './inputs/ButtonInput';
 
 const container = new PIXI.Container();
-
 export const app = new PIXI.Application({
   resizeTo: window,
-  backgroundColor: PIXI.utils.string2hex("#f0f9db"),
+  backgroundColor: PIXI.utils.string2hex('#f0f9db'),
 });
 document.body.appendChild(app.view);
 
 app.stage.addChild(container);
 app.stage.addChild(particleContainer);
 
-let debugMode = true;
-const debugInput = document.body.querySelector("#debug");
-debugInput.checked = debugMode;
-debugInput.onchange = () => {
-  debugMode = debugInput.checked;
-  flamableArea.setDebug(debugMode);
+const debugInput = new CheckboxInput('Show temperature (˚C)', false);
+debugInput.onInput = (value) => {
+  flammableArea.setDebug(value);
 };
 
-const settings = new SettingsHandlers();
+const forestDensityInput = new RangeInput('Forest density:', 0.05, 1, 0.05, 0.8);
+forestDensityInput.onInput = (value) => {
+  generateNewForest(value);
+};
+const heatRadianceInput = new RangeInput('Heat radiance:', 50, 500, 10, 250);
+heatRadianceInput.onInput = (value) => {
+  flammableArea.heatRadiance = value;
+};
 
+const resetButton = new ButtonInput('Reset to defaults');
+resetButton.onClick = () => {
+  forestDensityInput.reset();
+  heatRadianceInput.reset();
+  debugInput.reset();
+};
+
+const newForestButton = new ButtonInput('Generate new forest');
+newForestButton.onClick = () => {
+  generateNewForest(forestDensityInput.input.value);
+};
 
 const forestGen = new ForestGenerator({
   width: window.innerWidth,
   height: window.innerHeight,
 });
-export const flamableArea = new FlammableArea(forestGen.trees);
 
-const generateForestButton = document.body.querySelector("#generate");
+export const flammableArea = new FlammableArea(forestGen.trees);
 
-generateForestButton.addEventListener("click", () => {
-  forestGen.density = settings.forestDensity
-  forestGen.generateTrees();
-  flamableArea.cleanFlamables();
-  flamableArea.setFlamables(forestGen.trees);
-  container.removeChildren();
-  particleContainer.removeChildren();
-  container.addChild(...flamableArea.flamables);
-});
-
-container.addChild(...flamableArea.flamables);
-
-
+container.addChild(...flammableArea.flamables);
 
 app.ticker.add((delta) => {
   gameLoop();
 });
+
+function generateNewForest(density) {
+  forestGen.density = density;
+  forestGen.debug = settings.debugMode;
+  forestGen.generateTrees();
+  flammableArea.cleanFlamables();
+  flammableArea.setFlamables(forestGen.trees);
+  container.removeChildren();
+  particleContainer.removeChildren();
+  container.addChild(...flammableArea.flamables);
+}
